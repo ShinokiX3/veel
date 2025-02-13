0952f512b39c8b90d1ad5245e2f0f62ce097868b
ShinokiX3 <endernasha85@gmail.com>
2025-02-13 19:56:26 +0200

    add: setup colors for theme in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,13 +4,18 @@ export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "var(--primary)",
+        primaryLight: "var(--primary-light)",
+        primaryDark: "var(--primary-dark)",
+        muted: "var(--muted)",
+        mutedDark: "var(--muted-dark)",
       },
     },
   },
