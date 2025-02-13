7c42e57338b8712a83afec744997d571737fc428
ShinokiX3 <endernasha85@gmail.com>
2025-02-13 20:26:26 +0200

    add: page entry & layout, query provider & handle not found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Stub from '@/components/shared/common/stub';
+import React from 'react';
+
+const NotFound = () => {
+    return <div className='flex items-center justify-center h-screen w-screen'>
+        <Stub passage='404 Page Not Found' />;
+    </div>
+};
+
+export default NotFound;
